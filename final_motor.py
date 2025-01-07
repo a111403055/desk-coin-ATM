012a749958fab8b5f1ca6375d057fc021b0a5dd7
a111403055 <119419069+a111403055@users.noreply.github.com>
2025-01-08 01:46:17 +0800

    Add files via upload

---
+++ final_motor.py
@@ -0,0 +1,17 @@
+from gpiozero import Servo
+from time import sleep
+
+a = from tgbot import number
+b = from tgbot import dark_counts 
+
+
+print(a)
+print(b)
+
+#servo = Servo(17)
+
+#while True:
+#    servo.min()
+#    sleep(2)
+#    servo.max()
+#    sleep(2)
