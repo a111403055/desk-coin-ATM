f0b8eb60069c58bf41c93f7fa1d49ec0798a29a8
a111403055 <119419069+a111403055@users.noreply.github.com>
2025-01-08 20:42:15 +0800

    Update final_motor.py

--- final_motor.py
+++ final_motor.py
@@ -8,6 +8,20 @@ b = from tgbot import dark_counts
 print(a)
 print(b)
 
+for i in range(len(b)):
+    while a > 0 and b[i] > 0:
+      if i == 0:
+          if a >= b[i]:
+              a -= 50
+              b[i] -= 1
+          else:
+              break
+return a, b
+
+while a > 0:
+  if b[3] >= 1:
+    a - 50
+
 #servo = Servo(17)
 
 #while True:
